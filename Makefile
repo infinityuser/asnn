c16e2fecd0dbf6ec2c148df48321ae27ab63e912
Bogdan <kerpler7563@gmail.com>
2020-08-06 15:35:53 +0000

    fixed architecture

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
-
 build:
 	g++ -c ./src/build.cpp -O3
 	mv build.o kermdl.o 
-
--- src/build.cpp
+++ src/build.cpp
@@ -2,6 +2,6 @@
 #include <string>
 #include <cmath>
 
-#include "kernel.hpp"
+#include "../kernel.hpp"
 #include "kernel.cpp"
 #include "exec.cpp"
